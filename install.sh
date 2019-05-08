e542cdc221b5bcbcfaeaff8f865efcaa4730d8ba
byplayer <byplayer100@gmail.com>
2019-05-08 15:41:46 +0800

    add install script

--- install.sh
+++ install.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-source $HOME/.nvm/nvm.sh
+NVM_VER=v0.34.0
 
-set -ex
+pushd `dirname ${0}` > /dev/null
+BASE_DIR=`pwd`
+popd > /dev/null
+
+
+pushd $BASE_DIR >> /dev/null
+
+
+if [ ! -d .nvm ]; then
+  git clone https://github.com/creationix/nvm.git .nvm
+fi
+
+pushd .nvm >> /dev/null
+git checkout $NVM_VER
+popd >> /dev/null
+
+export NVM_DIR=${BASE_DIR}/.nvm
+source ${NVM_DIR}/nvm.sh
+
+# install node if ncessary
+NODE_VER=$(cat .nvmrc| head -1)
+nvm ls ${NODE_VER}
+result=$?
+
+if [ $result -ne 0 ]; then
+  nvm install $(cat .nvmrc| head -1)
+fi
 
 nvm use
 
-npm install
+set -ex
+
+npm install -g prettier
+
+for bin_file in $(\ls .nvm/versions/node/${NODE_VER}/bin); do
+  cp bin_templ bin/$bin_file
+done
+
+popd >> /dev/null
 
 echo "install completed"
