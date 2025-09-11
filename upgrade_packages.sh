a69be6b97f3b55d41f6472f801376f3265f3d94f
byplayer <byplayer100@gmail.com>
2025-09-11 09:45:16 +0900

    ⬆️ upgrade packages

--- upgrade_packages.sh
+++ upgrade_packages.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-git switch -c update_packages_$(date +"%Y%m%d%H%M%S")
-
 yarn upgrade --latest
+
+if [[ -z $(git status --porcelain) ]]; then
+    echo "✅ No changes to commit. Exiting."
+    exit 0
+fi
+
+git switch -c "update_packages_$(date +"%Y%m%d%H%M%S")"
 
 git add .
 git commit -m "⬆️ upgrade packages"
